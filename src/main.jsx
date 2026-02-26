4bef8318ab123b53a879e144ee65fc60a5d13faf
Jennifer Román <jenniferromanmuerte@gmail.com>
2026-02-26 11:51:30 +0100

    Initial React Vite setup with SCSS structure

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./styles/main.scss";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
